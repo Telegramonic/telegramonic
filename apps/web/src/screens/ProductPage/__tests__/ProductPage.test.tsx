f5f30b4af54b9a2e874354290bca1994f3a82847
Amit Raikwar <raikwar.amit.git@gmail.com>
2026-06-09 12:07:02 +0530

    test(TEL-24): mock desktop package.json version in ProductPage test

--- apps/web/src/screens/ProductPage/__tests__/ProductPage.test.tsx
+++ apps/web/src/screens/ProductPage/__tests__/ProductPage.test.tsx
@@ -1,6 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { system } from '@components';
+
+jest.mock('../../../../../desktop/package.json', () => ({
+  version: '0.2.1',
+}));
+
 import ProductPage from '../ProductPage';
 
 const renderProductPage = () => {
